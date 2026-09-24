421c70610b7323a2864ce119cd7a2f11ea2cc1c6
Christian-Mobo-Maileva <x.maileva@gmail.com>
2026-09-24 18:51:16 -0400

    mi primer commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+// Conexión usando variables de entorno pasadas por Docker Compose
+const pool = new Pool({
+  host: process.env.DB_HOST || 'db',
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  port: 5432,
+});
+
+app.get('/api/datos', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({
+      mensaje: "¡Conexión exitosa desde el Backend exisoto666565!",
+      db_time: result.rows[0].now
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Backend escuchando en puerto ${PORT}`);
+});   
